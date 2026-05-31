5587003d03baf52eff70ff754a678c40386c26c1
Shubh <rathodshubham7711@gmail.com>
2026-05-31 15:02:24 +0530

    User model & methods created

--- src/models/User.js
+++ src/models/User.js
@@ -39,9 +39,15 @@ const userSchema = new mongoose.Schema(
   },
 );
 
+//Hashing Password before Saving
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
 });
+
+//Custom method in the Schema for Check the password is correct or Not
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
